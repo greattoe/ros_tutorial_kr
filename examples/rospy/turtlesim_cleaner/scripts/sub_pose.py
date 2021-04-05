691ef85ebce5be67dcaa83ec6538d3cce979de68
이용진 <greattoe@gmail.com>
2021-04-05 10:37:57 +0900

    modified:   examples/rospy/turtlesim_cleaner/scripts/sub_pose.py

--- examples/rospy/turtlesim_cleaner/scripts/sub_pose.py
+++ examples/rospy/turtlesim_cleaner/scripts/sub_pose.py
@@ -5,9 +5,9 @@ from turtlesim.msg import Pose
 from math import degrees
 
 def get_pose(dat):
-    x  = round(dat.x) 
-    y  = round(dat.y)
-    th = round(degrees(dat.theta))
+    x  = round(dat.x, 2) 
+    y  = round(dat.y, 2)
+    th = round(degrees(dat.theta), 2)
     rospy.loginfo("x = %s(m), y = %s(m), th = %s(deg)", x, y, th)
 
 def sub_turtle_pose():
