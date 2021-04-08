869b6eb0992432d321aa862b445713b5b224f624
이용진 <greattoe@gmail.com>
2021-04-08 09:08:35 +0900

    modified:   examples/rospy/tb3_cleaner/scripts/8_align2marker.py

--- examples/rospy/tb3_cleaner/scripts/8_align2marker.py
+++ examples/rospy/tb3_cleaner/scripts/8_align2marker.py
@@ -2,98 +2,74 @@
 
 import rospy
 from turtlesim.msg import Pose
-from math import pi
+from math import degrees, pi
 from geometry_msgs.msg import Twist
 from ar_track_alvar_msgs.msg import AlvarMarkers
 
+# change id of your marker
 TARGET_ID = 13
 
 # Turtlebot3 Specification
 MAX_LIN_SPEED =  0.22
 MAX_ANG_SPEED =  2.84
 
+# set parking zone( kind of tolerlance )
+MAX_DIST = 0.20
+MIN_DIST = 0.15
 
-class AlignMarker:
+
+class KeepDist:
 
     def __init__(self):
     
-        rospy.init_node('align_to_marker', anonymous = True)       
-        rospy.Subscriber('/tb3pose', Pose, self.get_theta)
-        rospy.Subscriber('/ar_pose_marker', AlvarMarkers, self.get_marker)
+        rospy.init_node('tb3_keep_dist', anonymous = True)        
+        rospy.Subscriber('/ar_pose_marker', AlvarMarkers, self.get_marker )
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)
-        
         self.tw = Twist()
-        self.tw.angular.z = MAX_ANG_SPEED * 0.1
-        
-        self.curr_th   = 0
-        self.find_th   = 0
-        self.lost_th   = 0
-        
-        self.is_1st_find    = True
-        self.is_1st_lost    = True
-        self.align_finished = False
-             
-        
-    def get_theta(self, dat):
-        self.curr_th = dat.theta 
+        self.dist = 0
+        self.count = 0
+        self.lin_speed = MAX_LIN_SPEED * 0.25
         
         
     def get_marker(self, msg):
-        
-        self.pub.publish(self.tw)
-        
+    
         if len(msg.markers) > 0:
           
             for msg in msg.markers:
             
                 if msg.id == TARGET_ID:
                 
-                    print "found target marker"
-                
-                    if  self.is_1st_find == True:
-                        self.find_th  = self.curr_th
-                        print "get theta to start recognize marker."
-                        self.is_1st_find  = False
+                    self.dist = msg.pose.pose.position.z * 
+                    self.move2marker()
                 else:
-                    print "id mismatch"
+                    print "wrong marker id!"
+        else:
+            print "marker not found!"
                 
-        else: # lost marker            
-                
-            print "lost marker"
+    
+    def move2marker(self):
         
-            if  self.is_1st_find == False and self.is_1st_lost == True:
-                self.lost_th = self.curr_th
-                print "get theta to end recognize marker."
-                self.is_1st_lost = False
-                
-                if self.align_finished == False:
-                    self.align2marker()
-                    self.align_finished = True
-                
-                
-    def align2marker(self):
+        # marker
+        #   |        min    max
+        #   |   --->  |      |  <---
+        #   +---------+------+----------------
+        #    backward   stop   forward 
         
-        self.tw.angular.z = 0
-        self.pub.publish(self.tw)
+        if   self.dist > MAX_DIST:
+            self.tw.linear.x =  self.lin_speed
+        elif self.dist < MIN_DIST:
+            self.tw.linear.x = -self.lin_speed
+        else:
+            self.tw.linear.x = 0
         
-        current = self.curr_th        
-        target = current - abs(self.find_th - self.lost_th) * 0.7
-        
-        print "get target theta & start rotating."
-        self.tw.angular.z = -MAX_ANG_SPEED * 0.125        
-        self.pub.publish(self.tw)
-        
-        while self.curr_th > target:  pass
-        
-        self.tw.angular.z = 0
-        self.pub.publish(self.tw)
-        print "align complete."
+        self.pub.publish(self.tw)        
+        print "distance to marker = %f(cm)" %(self.dist * 100)
           
 
 if __name__ == '__main__':
     try:
         
-        AlignMarker()
+        KeepDist()
         rospy.spin()
         
     except rospy.ROSInterruptException:  pass
