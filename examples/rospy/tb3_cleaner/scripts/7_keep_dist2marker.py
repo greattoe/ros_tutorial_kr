a2a6920dc1c409837db722ffb309c8e5ed4f51bf
이용진 <greattoe@gmail.com>
2021-04-08 14:37:02 +0900

    modified:   examples/rospy/tb3_cleaner/scripts/7_keep_dist2marker.py

--- examples/rospy/tb3_cleaner/scripts/7_keep_dist2marker.py
+++ examples/rospy/tb3_cleaner/scripts/7_keep_dist2marker.py
@@ -5,8 +5,8 @@ from turtlesim.msg import Pose
 from math import degrees, pi
 from geometry_msgs.msg import Twist
 from ar_track_alvar_msgs.msg import AlvarMarkers
+from tf.transformations import euler_from_quaternion
 
-# change id of your marker
 TARGET_ID = 13
 
 # Turtlebot3 Specification
@@ -17,57 +17,114 @@ MAX_ANG_SPEED =  2.84
 MAX_DIST = 0.20
 MIN_DIST = 0.15
 
-
-class KeepDist:
+class Move2Marker:
 
     def __init__(self):
     
-        rospy.init_node('tb3_keep_dist', anonymous = True)        
-        rospy.Subscriber('/ar_pose_marker', AlvarMarkers, self.get_marker )
+        rospy.init_node('align_to_marker', anonymous = True)       
+        rospy.Subscriber('/tb3pose', Pose, self.get_theta)
+        rospy.Subscriber('/ar_pose_marker', AlvarMarkers, self.get_marker)
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)
-        self.tw = Twist()
-        self.dist = 0
-        self.count = 0
+        
+        self.ang_speed = MAX_ANG_SPEED * 0.100
         self.lin_speed = MAX_LIN_SPEED * 0.125
+        
+        self.tw = Twist()
+        self.tw.angular.z = self.ang_speed
+        
+        self.curr_th   = 0
+        self.find_th   = 0
+        self.lost_th   = 0
+        self.dist      = 0
+        
+        self.is_1st_find     = True
+        self.is_1st_lost     = True
+        self.align_finished  = False
+        self.misson_complete = False
+             
+        
+    def get_theta(self, dat):
+        self.curr_th = dat.theta 
         
         
     def get_marker(self, msg):
-    
+        
+        self.pub.publish(self.tw)
+        
         if len(msg.markers) > 0:
           
             for msg in msg.markers:
             
                 if msg.id == TARGET_ID:
-                    
-                    self.dist = msg.pose.pose.position.z
-                    self.move2marker()
-                else:
-                    print "wrong marker id!"
-        else:
-            print "marker not found!"
-        
                 
-    
-    def move2marker(self):
+                    print "found target marker"
+                    self.dist = msg.pose.pose.position.z                    
+                
+                    if  self.is_1st_find == True:
+                        self.find_th  = self.curr_th
+                        print "get theta to start recognize marker."
+                        self.is_1st_find  = False
+                        
+                    if self.align_finished == True and self.misson_complete == False:
+                        self.toward2marker()
+                    
+                    
+                else:
+                    print "id mismatch"
+                
+        else: # lost marker            
+                
+            print "lost marker"
         
+            if self.is_1st_find == False and self.is_1st_lost == True:
+                self.lost_th = self.curr_th
+                print "get theta to end recognize marker."
+                self.is_1st_lost = False
+                
+                if self.align_finished == False:
+                    self.align2marker()
+                    self.align_finished = True
+                
+                
+    def align2marker(self):
+        
+        self.tw.angular.z = 0
+        self.pub.publish(self.tw)
+        
+        current = self.curr_th        
+        target = current - abs(self.find_th - self.lost_th) * 0.7
+        
+        print "get target theta & start rotating."
+        self.tw.angular.z = -MAX_ANG_SPEED * 0.125        
+        self.pub.publish(self.tw)
+        
+        while self.curr_th > target:  pass
+        
+        self.tw.angular.z = 0
+        self.pub.publish(self.tw)
+        print "align complete."
+        
+        
+    def toward2marker(self):
+        '''
         # marker
         #   |        min    max
         #   |   --->  |      |  <---
         #   +---------+------+----------------
         #    backward   stop   forward 
-        
-        speed = MAX_LIN_SPEED * 0.15
+        '''
+        speed = MAX_LIN_SPEED * 0.125
         
         if   self.dist > MAX_DIST:
-            if self.dist - MAX_DIST > 20:
-                self.tw.linear.x =  speed * 1.0
+            if self.dist - MAX_DIST > 0.20:
+                self.tw.linear.x =  speed * 2.0
             else:
-                self.tw.linear.x =  speed * 0.75
-        elif self.dist < MIN_DIST:        
+                self.tw.linear.x =  speed * 0.55
+        elif self.dist < MIN_DIST:
             self.tw.linear.x =  speed * 0.45 * -1
             
         else:
-            self.tw.linear.x = 0
+            self.tw.linear.x = 0;   self.pub.publish(self.tw) 
         
         self.pub.publish(self.tw)        
         print "distance to marker = %f(cm)" %(self.dist * 100)
@@ -75,9 +132,7 @@ class KeepDist:
 
 if __name__ == '__main__':
     try:
-        
-        tb3 = KeepDist()
-        tb3.move2marker()
+        Move2Marker()        
         rospy.spin()
         
     except rospy.ROSInterruptException:  pass
